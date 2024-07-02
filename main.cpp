f3863facca57bd319d3537434a4fe5ff164cd5e5
MariaK96 <knbmaria@gmail.com>
2024-07-02 23:31:04 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/* μοντουλο */
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+    int x=3,y;
+    cout<<"δωσε μου το χ\n";
+    cin>>x;
+    y=x%2;
+    y==0 ? cout<<"to x einai artios" : cout<<"to x einai peritos";
+}
